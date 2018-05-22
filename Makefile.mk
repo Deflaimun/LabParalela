c2db2e825e6d4c2f7fd8c8bcb2307b9cfd668472
Deflaimun <paulohtb@hotmail.com>
2018-05-22 10:36:53 -0300

    Organizando repo

--- Makefile.mk
+++ Makefile.mk
@@ -1,21 +1,21 @@
 make: copy_ssh 
 
 comp-lab1: @echo Compilando lab1 
-		   mpicc lab1 -o lab1
+		   mpicc ex1.c -o lab1
 
 comp-lab2: @echo Compilando lab1 
-			mpicc lab2 -o lab2
+			mpicc ex2.c -o lab2
 
 comp-lab3: @echo Compilando lab1 
- 			mpicc lab3 -o lab3
+ 			mpicc ex3.c -o lab3
 
 run-lab1: @echo Rodando lab1   	
 		  mpirun --hostifle nome_hosts -np 4 -mca plm_rsh_no_tree_spawn 1 lab1
 
-run-lab2:  @echo Compilando lab2
+run-lab2:  @echo Rodando lab2
 			mpirun --hostifle nome_hosts -np $(n) -mca plm_rsh_no_tree_spawn 1 lab2
 
-run-lab3: @echo Compilando lab1 
+run-lab3: @echo Rodando lab3 
 		  mpirun --hostifle nome_hosts -np $(n) -mca plm_rsh_no_tree_spawn 1 lab3
 
 copy-ssh: @echo Fazendo copia ssh 
